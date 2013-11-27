6dd13423dca8a1e5abed0290dbf2e20ae49bcba5
Paul Reilly <paldiutheminecrafter@gmail.com>
2013-11-27 16:10:13 -0500

    Saving for formatting.

---
+++ src/me/creepercoders/FreedomOpMod/FOM_Util.java
@@ -0,0 +1,11 @@
+package me.creepercoders.FreedomOpMod;
+
+import org.bukkit.entity.Player;
+import org.bukkit.Bukkit;
+import org.bukkit.Server;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.Command;
+
+public class FOM_Util
+{
+}
